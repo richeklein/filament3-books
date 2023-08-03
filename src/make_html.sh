7bbbf33299c1ce9a241936bf7831e0457f7119cd
Richard Klein <rklein99@gmail.com>
2023-08-03 15:22:40 -0400

    add scripts

---
+++ src/make_html.sh
@@ -0,0 +1,6 @@
+paste merge_order.txt titles.txt | while IFS=$'\t' read -r file title
+do
+    echo "# $title" | cat - "$file" >> temp.md
+done
+pandoc -s --toc temp.md -o filament3-book.html -M title="FilamentPHP 3 Documentation" -M author="FilamentPHP Team"
+rm temp.md
